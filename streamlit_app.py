75d6418a8dbc71a37954f1183f8fcbd933cb5d40
djkoogy <djkoogy@gmail.com>
2022-10-24 19:27:12 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,3 +15,7 @@ for uploaded_file in uploaded_files:
 
 view = uploaded_file
 
+col1, col2, col3 = st.columns(3)
+col1.metric("Temperature", "70 °F", "1.2 °F")
+col2.metric("Wind", "9 mph", "-8%")
+col3.metric("Humidity", "86%", "4%")
